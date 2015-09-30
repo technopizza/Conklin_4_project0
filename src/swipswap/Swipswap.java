722c54e3b9acd0dafaf7d7482b74bf3503718954
Jason Conklin <jasonconklin0@gmail.com>
2015-09-30 07:45:33 -0500

    first save of swipswap

---
+++ src/swipswap/Swipswap.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swipswap;
+
+/**
+ *
+ * @author jconklin2391
+ */
+public class Swipswap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    boolean isDivisibleByTwo(int number) {
+        return (number % 2 == 0);
+    }
+    boolean isDivisibleByThree(int number) {
+        return (number % 3 == 0);
+    }
+    boolean isDivisibleByFive(int number) {
+        return (number % 5 == 0);
+    }
+
+}
